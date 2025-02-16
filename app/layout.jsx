4ebe774c362322701267ddad3b4523a5c05b7b68
Martin Spassov <mspassov@uwo.ca>
2025-02-16 17:39:52 -0500

    basic layout structure and cleanup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import "@/assets/global.css";
+import "@/assets/nav.css";
+
+export const metadata = {
+  title: "Martin Spassov | Portfolio",
+  keywords: "Martin, Spassov, Portfolio, Personal, Website, Product Management",
+};
 
 const MainLayout = ({ children }) => {
   return (
