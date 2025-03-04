043995c3e1746b6c7fcc896454db928fbd946508
Martin Spassov <mspassov@uwo.ca>
2025-03-03 22:44:27 -0500

    Added Buttons to cards, and began web dev section

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { FaCity, FaCalendarAlt } from "react-icons/fa";
 
 const Experience = () => {
@@ -12,7 +13,7 @@ const Experience = () => {
             <div className="experience-card">
               <div className="card-image-container">
                 <Image
-                  src="/promotionImage.png"
+                  src="/postPurchase.png"
                   width={350}
                   height={0}
                   alt="ecommerce promotion"
@@ -37,12 +38,19 @@ const Experience = () => {
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Delectus, similique.
                 </p>
+                <Link
+                  href="/experience/post-purchase-platform"
+                  className="link"
+                  id="details-link"
+                >
+                  <div className="btn">Details</div>
+                </Link>
               </div>
             </div>
             <div className="experience-card">
               <div className="card-image-container">
                 <Image
-                  src="/postPurchase.png"
+                  src="/promotionImage.png"
                   width={350}
                   height={0}
                   alt="ecommerce promotion"
@@ -67,6 +75,13 @@ const Experience = () => {
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Delectus, similique.
                 </p>
+                <Link
+                  href="/experience/promotion-engine"
+                  className="link"
+                  id="details-link"
+                >
+                  <div className="btn">Details</div>
+                </Link>
               </div>
             </div>
             <div className="experience-card">
@@ -97,6 +112,13 @@ const Experience = () => {
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Delectus, similique.
                 </p>
+                <Link
+                  href="/experience/wire-payment-system"
+                  className="link"
+                  id="details-link"
+                >
+                  <div className="btn">Details</div>
+                </Link>
               </div>
             </div>
           </div>
@@ -105,95 +127,77 @@ const Experience = () => {
 
       <section className="experience">
         <div className="container">
-          <h2>Web Development Experience</h2>
-          <div className="experience-flex">
-            <div className="experience-card">
-              <div className="card-image-container">
-                <Image
-                  src="/promotionImage.png"
-                  width={350}
-                  height={0}
-                  alt="ecommerce promotion"
-                  className="card-image"
-                />
-              </div>
-              <div className="card-text">
-                <h3>Post-Purchase Platform</h3>
-                <div className="card-details">
-                  <FaCity className="details-icon" />
-                  <p>
-                    <strong>Company: </strong>Passport Global
-                  </p>
-                </div>
-                <div className="card-details">
-                  <FaCalendarAlt className="details-icon" />
-                  <p>
-                    <strong>Date: </strong>2023 - 2024
-                  </p>
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Delectus, similique.
-                </p>
-              </div>
-            </div>
-            <div className="experience-card">
-              <div className="card-image-container">
+          <h2>Web Development Projects</h2>
+          <div className="projects-grid">
+            <div className="project-card">
+              <div className="left-img">
                 <Image
                   src="/postPurchase.png"
-                  width={350}
+                  alt="test-for-now"
+                  width={250}
                   height={0}
-                  alt="ecommerce promotion"
-                  className="card-image"
+                  className="project-img"
                 />
               </div>
-              <div className="card-text">
-                <h3>Promotion Engine Microservice</h3>
-                <div className="card-details">
-                  <FaCity className="details-icon" />
-                  <p>
-                    <strong>Company: </strong>Deloitte
-                  </p>
-                </div>
-                <div className="card-details">
-                  <FaCalendarAlt className="details-icon" />
-                  <p>
-                    <strong>Date: </strong>2022 - 2023
-                  </p>
-                </div>
+              <div className="right-text">
+                <h3>PropertyPulse</h3>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Delectus, similique.
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quisquam, vitae!
                 </p>
               </div>
             </div>
-            <div className="experience-card">
-              <div className="card-image-container">
+            <div className="project-card">
+              <div className="left-img">
                 <Image
-                  src="/banking.png"
-                  width={350}
+                  src="/postPurchase.png"
+                  alt="test-for-now"
+                  width={250}
                   height={0}
-                  alt="ecommerce promotion"
-                  className="card-image"
+                  className="project-img"
                 />
               </div>
-              <div className="card-text">
-                <h3>Wire Payment System</h3>
-                <div className="card-details">
-                  <FaCity className="details-icon" />
-                  <p>
-                    <strong>Company: </strong>Deloitte
-                  </p>
-                </div>
-                <div className="card-details">
-                  <FaCalendarAlt className="details-icon" />
-                  <p>
-                    <strong>Date: </strong>2021 - 2022
-                  </p>
-                </div>
+              <div className="right-text">
+                <h3>PropertyPulse</h3>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Delectus, similique.
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quisquam, vitae!
+                </p>
+              </div>
+            </div>
+            <div className="project-card">
+              <div className="left-img">
+                <Image
+                  src="/postPurchase.png"
+                  alt="test-for-now"
+                  width={250}
+                  height={0}
+                  className="project-img"
+                />
+              </div>
+              <div className="right-text">
+                <h3>PropertyPulse</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quisquam, vitae!
+                </p>
+              </div>
+            </div>
+            <div className="project-card">
+              <div className="left-img">
+                <Image
+                  src="/postPurchase.png"
+                  alt="test-for-now"
+                  width={250}
+                  height={0}
+                  className="project-img"
+                />
+              </div>
+              <div className="right-text">
+                <h3>PropertyPulse</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quisquam, vitae!
                 </p>
               </div>
             </div>
